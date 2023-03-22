711a17ccb713cddd640a2b5c79960a9f9ba956c4
Nathan Shaw <nathan.shaw@capgemini.com>
2023-03-22 09:18:39 +0000

    revert back to original inline arrow fn

--- krystal-dogs/src/pages/DogGenerator.jsx
+++ krystal-dogs/src/pages/DogGenerator.jsx
@@ -28,7 +28,7 @@ const DogGenerator = () => {
       </button>
       <button
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-        onClick={addFavourite(dogs, setFavourite)}
+        onClick={() => addFavourite(dogs, setFavourite)}
       >
         Add to favourites
       </button>
